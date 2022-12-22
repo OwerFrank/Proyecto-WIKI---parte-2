be39fa00cdcbddc4511b5cb0349260d863d997eb
OwerFrank <110862686+OwerFrank@users.noreply.github.com>
2022-12-22 17:51:28 -0500

    Segundo Avance de register.pl
    Nos conectamos con la base de datos y hacemos el cuerpo.

--- register.pl
+++ register.pl
@@ -32,3 +32,52 @@ if(validarArray(@parametros)==4){
 }else{
   print renderXML(@salida);
 }
+
+#print STDERR "$userName\n$password\n$firstName\n$lastName";
+sub insertaBD{
+  my @campos =@_;
+  my $user = 'alumno';
+  my $password = 'pweb1';
+  my $dsn = 'DBI:MariaDB:database=pweb1;host=192.168.1.23';
+  my $dbh = DBI->connect($dsn, $user, $password) or die
+  die("No se pudo conectar!");
+  my $sql = "INSERT INTO Users(userName,password,firstName,lastName)VALUES(?,?,?,?)";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($campos[0],$campos[1],$campos[2],$campos[3]);
+  $sth->finish;
+  $dbh->disconnect;
+
+}
+
+sub renderCuerpo{
+  my @linea = @_;
+  my $cuerpo = <<"CUERPO";
+      <owner>$linea[0]</owner>
+      <firstName>$linea[2]</firstName>
+      <lastName>$linea[3]</lastName>
+CUERPO
+  return $cuerpo;
+}
+
+sub renderXML{
+  my $cuerpoxml = $_[0];
+  my $xml = <<"XML";
+<?xml version='1.0' encoding= 'utf-8'?>
+  <user>
+  $cuerpoxml
+  </user>
+XML
+  return $xml
+}
+
+sub validarArray{
+  my @array = @_;
+  my $contador=0;
+  foreach my $elemento(@array){
+    if (defined($elemento)){
+      $contador++;
+    }
+  }
+  return $contador;
+}
+
